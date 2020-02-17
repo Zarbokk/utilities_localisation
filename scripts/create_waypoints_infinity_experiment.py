655a41d10ea7b228f22578a34824cec72240a0cb
Your Name <you@example.com>
2020-02-17 17:01:46 +0100

    scp command

--- scripts/create_waypoints_infinity_experiment.py
+++ scripts/create_waypoints_infinity_experiment.py
@@ -314,7 +314,7 @@ def change_parameter():
         wanted_z_position = 0.5
         distance_to_point = 0.8
         thrust = 0.1
-        do_roll = True
+        do_roll = False
     if current_parameters == 2:
         R = 0.5
         wanted_z_position = 0.7
