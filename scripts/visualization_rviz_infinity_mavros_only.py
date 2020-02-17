ff786603717a3facd2ddd47541f19e21723383c8
zarbokk <timhansen93@googlemail.com>
2020-02-17 13:20:31 +0100

    update_noise_barometer

--- scripts/visualization_rviz_infinity_mavros_only.py
+++ scripts/visualization_rviz_infinity_mavros_only.py
@@ -13,9 +13,6 @@ from mavros_msgs.msg import PositionTarget, AttitudeTarget
 # import matplotlib.pyplot as plt
 import csv
 
-# publisher_waypoint = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
-# publisher_waypoint = rospy.Publisher('/mavros/setpoint_raw/local', PositionTarget, queue_size=1)
-publisher_waypoint = rospy.Publisher('mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=1)
 publisher_marker = rospy.Publisher('/infinity', MarkerArray, queue_size=1)
 current_pos_number = 10
 N = 100
@@ -157,7 +154,6 @@ def pathplanning(current_waypoint, current_position_boat):
     #return current_waypoint
     return point_to_be_controlled_on
 
-
 def visualization():
     r = 0.05
     markerArray = MarkerArray()
@@ -194,120 +190,18 @@ def visualization():
             marker.pose.position.y = p[2, i]  # y
             marker.pose.position.z = wanted_z_position  # z
             markerArray.markers.append(marker)
-    for i in range(len(current_path[0])):
-        marker = Marker()
-        marker.header.frame_id = "global_tank"
-        marker.id = i + p.shape[1]
-        marker.type = marker.SPHERE
-        marker.action = marker.ADD
-        marker.scale.x = r  # r*2 of distance to camera from tag_14
-        marker.scale.y = r
-        marker.scale.z = r
-        marker.color.g = 1
-        marker.color.a = 1  # transparency
-        marker.pose.orientation.w = 1.0
-        marker.pose.position.x = current_path[0,i]  # x
-        marker.pose.position.y = current_path[1,i]  # y
-        marker.pose.position.z = wanted_z_position  # z
-        markerArray.markers.append(marker)
-    marker = Marker()
-    marker.header.frame_id = "global_tank"
-    marker.id = 200
-    marker.type = marker.SPHERE
-    marker.action = marker.ADD
-    marker.scale.x = r  # r*2 of distance to camera from tag_14
-    marker.scale.y = r
-    marker.scale.z = r
-    marker.color.g = 1
-    marker.color.r = 1
-    marker.color.a = 1  # transparency
-    marker.pose.orientation.w = 1.0
-    marker.pose.position.x = current_path[0,10/2]  # x
-    marker.pose.position.y = current_path[1,10/2]  # y
-    marker.pose.position.z = wanted_z_position  # z
-    markerArray.markers.append(marker)
-
     publisher_marker.publish(markerArray)
 
 
-def callback(msg):
-    """"""
-    global current_pos_number, N, R, p, rate, thrust, carrot, yaw
-    current_pos = p[1:4, current_pos_number]
-    # look if next waypoint should be loaded
-    if np.sqrt(
-            (msg.pose.position.x - current_pos[0]) ** 2 + (msg.pose.position.y - current_pos[1]) ** 2) < R:  # define R
-        current_pos_number = current_pos_number + 1
-        if current_pos_number > N - 1:
-            current_pos_number = 0
-        current_pos = p[1:4, current_pos_number]
-
-    current_waypoint = pathplanning(current_pos, np.asarray([msg.pose.position.x, msg.pose.position.y]))
-    rviz = True
-    if rviz:
-        visualization()
-
-    rotation_body_frame = Quaternion(w=msg.pose.orientation.w,
-                                     x=msg.pose.orientation.x,
-                                     y=msg.pose.orientation.y,
-                                     z=msg.pose.orientation.z)
-    yaw_current, pitch_current, roll_current = rotation_body_frame.inverse.yaw_pitch_roll
-
-    yaw_current = -yaw_current
-    pitch_current = -pitch_current
-    roll_current = -((roll_current + 360 / 180.0 * np.pi) % (np.pi * 2) - 180 / 180.0 * np.pi)
-    yaw_des = np.arctan2((current_waypoint[1] - msg.pose.position.y), (current_waypoint[0] - msg.pose.position.x))
-    pitch_des = -np.arctan((wanted_z_position - msg.pose.position.z) / distance_to_point)
-    # yaw_des = 0.0 / 180.0 * np.pi
-    # pitch_des = 0.0 / 180.0 * np.pi
-    roll_des = 0.0 / 180.0 * np.pi
-    qz_90n = Quaternion(
-        axis=[0, 0, 1], angle=-(yaw_des - np.pi / 2)) * Quaternion(axis=[0, 1, 0], angle=-pitch_des) * Quaternion(
-        axis=[1, 0, 0], angle=roll_des)
-
-    send_waypoint = AttitudeTarget()
-    send_waypoint.type_mask = 0
-    send_waypoint.orientation.x = qz_90n.x
-    send_waypoint.orientation.y = qz_90n.y
-    send_waypoint.orientation.z = qz_90n.z
-    send_waypoint.orientation.w = qz_90n.w
-    # print(qz_90n.x,qz_90n.y,qz_90n.z,qz_90n.w)
-    # 0.2 works
-    send_waypoint.thrust = thrust * np.cos(yaw_current - yaw_des) * np.cos(roll_current - roll_des) * np.cos(
-        pitch_current - pitch_des)
-    if abs(yaw_current - yaw_des) > np.pi / 2 or abs(roll_current - roll_des) > np.pi / 2 or abs(
-            pitch_current - pitch_des) > np.pi / 2:
-        send_waypoint.thrust = 0
-    publisher_waypoint.publish(send_waypoint)
-    rate.sleep()
 
 
 def main():
     rospy.init_node('waypoint_send')
     global rate, R, wanted_z_position, distance_to_point, thrust, carrot, yaw
-    rate = rospy.Rate(30)
     rate_2 = rospy.Rate(5)
-    rospy.Subscriber("/mavros/local_position/pose_NED", PoseStamped, callback, queue_size=1)
-
-    rospack = rospkg.RosPack()
-    data_path = rospack.get_path("utilities_localisation") + '/scripts/parameters.csv'
     while not rospy.is_shutdown():
         # while 1:
-        try:
-            with open(data_path, 'r') as f:
-                reader = csv.reader(f, delimiter=',')
-                # get header from first row
-                headers = next(reader)
-                # get all the rows as a list
-                data = list(reader)
-                # transform data into numpy array
-                data = np.array(data).astype(float)
-                R, wanted_z_position, distance_to_point, thrust, carrot, yaw = data[0]
-                yaw = yaw / 180 * np.pi
-
-                # print(R)
-        except:
-            pass
+        visualization()
         rate_2.sleep()
 
 
