df111d9958672fd88a3df1b3487dbeff740aaebf
Your Name <you@example.com>
2020-02-19 15:29:52 +0100

    use rosbag to track data in experiment

--- scripts/create_waypoints_infinity_experiment.py
+++ scripts/create_waypoints_infinity_experiment.py
@@ -17,7 +17,7 @@ import csv
 # publisher_waypoint = rospy.Publisher('/mavros/setpoint_raw/local', PositionTarget, queue_size=1)
 publisher_waypoint = rospy.Publisher('mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=1)
 publisher_marker = rospy.Publisher('/infinity', MarkerArray, queue_size=1)
-current_pos_number = 10
+current_pos_number = 0
 N = 100
 rate = None
 current_path = None
