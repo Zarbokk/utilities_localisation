411d272d8b32aba6da0975d57decdeb4f9a17b69
Your Name <you@example.com>
2020-02-23 19:03:36 +0100

    use rosbag to track data in experiment

--- scripts/create_waypoints_infinity_experiment.py
+++ scripts/create_waypoints_infinity_experiment.py
@@ -84,7 +84,7 @@ current_parameters = 0
 R = 0.5
 wanted_z_position = 0.5
 distance_to_point = 0.8
-thrust = 0.2
+thrust = 0.15
 carrot = 1
 roll_desired = 0
 p = create_inf()
@@ -313,25 +313,25 @@ def change_parameter():
         R = 0.5
         wanted_z_position = 0.5
         distance_to_point = 0.8
-        thrust = 0.2
+        thrust = 0.15
         do_roll = False
-    if current_parameters == 1 or current_parameters == 1 or current_parameters == 1:
+    if current_parameters == 1 or current_parameters == 2 or current_parameters == 3:
         R = 0.5
         wanted_z_position = 1
         distance_to_point = 0.8
-        thrust = 0.2
+        thrust = 0.15
         do_roll = False
     if current_parameters == 7 or current_parameters == 8 or current_parameters == 9:
         R = 0.5
         wanted_z_position = 0.9
         distance_to_point = 0.8
-        thrust = 0.2
+        thrust = 0.15
         do_roll = False
-    if current_parameters == 2:#WRONG DO 4
+    if current_parameters == 4:#WRONG DO 4
         R = 0.5
         wanted_z_position = 0.5
         distance_to_point = 0.8
-        thrust = 0.2
+        thrust = 0.15
         auftauchen = True
         do_roll = False
     return
