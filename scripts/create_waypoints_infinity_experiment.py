b3f8c390af5decb82ba4831698983cc993fb2217
Your Name <you@example.com>
2020-02-24 15:13:08 +0100

    use rosbag to track data in experiment

--- scripts/create_waypoints_infinity_experiment.py
+++ scripts/create_waypoints_infinity_experiment.py
@@ -292,7 +292,7 @@ def callback(msg):
         if roll_des > -np.pi / 3 and roll_des < 0:
             roll_des = 0
             do_roll = False
-        send_waypoint.thrust = thrust
+        send_waypoint.thrust = thrust*0.5
     # yaw_des = 0.0 / 180.0 * np.pi
     # pitch_des = 0.0 / 180.0 * np.pi
 
